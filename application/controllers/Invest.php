432ba6822bf85e1b4a45619426e110fed0f2ed9f
apri <apriyantotobing8@gmail.com>
2018-09-18 11:40:26 +0700

    masih sama

--- application/controllers/Invest.php
+++ application/controllers/Invest.php
@@ -1,5 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+include('src/SimpleImage.php');
+$img = new claviska\SimpleImage();
 class Invest extends CI_Controller {
 	function __construct(){
 		parent::__construct();		
