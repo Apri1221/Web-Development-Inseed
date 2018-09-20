1333c74edb5cd0515e6e2c8717a159dc438df267
apri <apriyantotobing8@gmail.com>
2018-09-20 11:39:26 +0700

    invoice

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -10,6 +10,13 @@ class Laporan extends CI_Controller {
     }
 
 	public function index() {
+		$username = $this->session->userdata('username');
+		$bank = $this->session->userdata('bank');
+		$nominal = $this->session->userdata('nominal');
+		$tglTarik = $this->session->userdata('tglTarik');
+		$noRek = $this->session->userdata('noRek');
+		$saldo = $this->session->userdata('saldo');
+
 		$pdf = new FPDF('p','mm','A4');
 
 		$pdf->AddPage();
@@ -24,28 +31,27 @@ class Laporan extends CI_Controller {
 		$pdf->Cell(130, 5, 'Jl. MT. Haryono',0,0);
 		$pdf->Cell(59, 5, '',0,1);
 
-		$pdf->Cell(130, 5, '[Malang, Indonesia, KodePos]',0,0);
-		$pdf->Cell(25, 5, 'Date',0,0);
-		$pdf->Cell(130, 5, '[dd/mm/yyyy]',0,1);
+		$pdf->Cell(113, 5, 'Malang, Indonesia, 65145',0,0);
+		$pdf->Cell(34, 5, "$tglTarik",0,1);
 
-		$pdf->Cell(130, 5, 'Phone [+62852614978xx]',0,0);
-		$pdf->Cell(25, 5, 'Customer Username',0,0);
-		$pdf->Cell(34, 5, '[namaAkun]',0,1);
-
-		$pdf->Cell(189, 10, '',0,1);
-
-		$pdf->Cell(189, 5, 'Tagihan kepada',0,0);
+		$pdf->Cell(130, 5, 'No. HP +62852614978xx',0,0);
+		$pdf->Cell(34, 5, "Mr. $username",0,1);
 
 		$pdf->SetFont('Arial','B',12);
-        $pdf->Cell(190,7,'DAFTAR SISWA KELAS IX JURUSAN REKAYASA PERANGKAT LUNAK',0,1,'C');
+		$pdf->Cell(189, 10, '',0,1);
+        $pdf->Cell(189,5,'Transaksi Penarikan',0,1);
         // Memberikan space kebawah agar tidak terlalu rapat
         $pdf->Cell(10,7,'',0,1);
         $pdf->SetFont('Arial','B',10);
-        $pdf->Cell(20,6,'NIM',1,0);
-        $pdf->Cell(85,6,'NAMA MAHASISWA',1,0);
-        $pdf->Cell(27,6,'NO HP',1,0);
-        $pdf->Cell(25,6,'TANGGAL LHR',1,1);
+        $pdf->Cell(20,6,'BANK',1,0);
+        $pdf->Cell(55,6,'Nomor Rekening',1,0);
+        $pdf->Cell(37,6,'Jumlah Penarikan',1,0);
+        $pdf->Cell(35,6,'Sisa Saldo',1,1);
         $pdf->SetFont('Arial','',10);
+        $pdf->Cell(20,6,"$bank",1,0);
+        $pdf->Cell(55,6,"$noRek",1,0);
+        $pdf->Cell(37,6,"$nominal",1,0);
+        $pdf->Cell(35,6,"$saldo",1,1);
         // $mahasiswa = $this->db->get('mahasiswa')->result();
         // foreach ($mahasiswa as $row){
         //     $pdf->Cell(20,6,$row->nim,1,0);
