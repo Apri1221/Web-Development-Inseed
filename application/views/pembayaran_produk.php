f88c8ba185afea692a60ed6fe370b48c46537584
apri <apriyantotobing8@gmail.com>
2018-09-20 23:19:50 +0700

    perbaiki cart nya pas detail pembayaran

--- application/views/pembayaran_produk.php
+++ application/views/pembayaran_produk.php
@@ -18,123 +18,110 @@
   <body>
     <!-- ISI NAVBAR DISINI YA!!!! -->
     <?php include_once("template/navbar.php"); ?>
-
-<section id="formPembayaranProduk">
-  <div class="container-fluid" style="padding: 5%;"> 
-    <div class="row">
-      <div class="col-md-7">
-         <h2 style="text-align: center;">Form Pembayaran Produk</h2>
-         <hr class="col-md-4">
-  <br>
-  <form class="form-horizontal" method = "post" action="<?php echo base_url();?>index.php/mart/thanks/<?php foreach ($this->cart->contents() as $items) : ?>
-	<?php echo $items['id'] ?> <?php endforeach ?>">
-    <div class="form-group">
-      <label class="control-label col-md-4" for="produk">Metode Pembayaran</label>
-      <div class="col-md-12">
-         <select class="form-control" id="sel1" placeholder="Metode Pembayaran">
-        <option>ATM</option>
-      </select>
-      </div>
-    </div>
-
-  <div class="form-group">
-      <label class="control-label col-md-4" for="deskripsi">Pilih Bank</label>
-        <div class="col-md-12">
-        <select class="form-control" id="sel1" placeholder="Kategori">
-		
-        <option>CIMB NIAGA</option>
-        <option>Mandiri</option>
-        <option>BCA</option>
-        <option>BRI</option>
-        <option>BNI</option>
-      </select>
-      </div>
-  </div>
- 
-
-
-
-  
-
-
-  <br>
-    <h2>Pengiriman</h2>
-   <br>
-   <div class="form-group">
-      <label class="control-label col-md-4" for="produk">Alamat Pengiriman</label>
-	  
-
-      <div class="col-md-12">
-        <textarea name="alamat" class="form-control"> </textarea>
-      </div>
-    </div>
-	
-	<div class="form-group">
-	<label class="control-label col-md-4" for="produk">Pilih Propinsi</label> 
-<div class="col-md-12">	
-		<select class="form-control" id="provinsi" placeholder="Jasa Pengiriman">
-		
-        <option>Jawa Timur</option>
-        <option>Jawa Barat</option>
-		<option>Jawa Tengah</option>
-        <option>DKI Jakarta</option>
-		 <option>DI Yogyakarta</option>
-        <option>Banten</option>
-		</select>
-		</div>
-		</div>
-	
-	<div class="form-group">
-      <label class="control-label col-md-4" for="produk">Pilih Jasa Pengiriman</label>
-      <div class="col-md-12">
-        <select class="form-control" id="sel1" placeholder="Jasa Pengiriman">
-        <option>JNE</option>
-        <option>TIKI</option>
-        <option>J&T</option>
-      </select>
-      </div>
-    </div>
-
-     <div class="form-group">
-      <label class="control-label col-md-4" for="produk">Paket Pengiriman</label>
-      <div class="col-md-12">
-         <select class="form-control" id="sel1" placeholder="Paket Pengiriman">
-        <option>REG</option>
-        <option>YES</option>
-        <option>OKE</option>
-      </select>
-      </div>
-    </div>
-  
-   
-    
-    <div class="form-group">        
-      <div class="col-sm-offset-2 col-md-12">
-<a href="<?php echo base_url();?>index.php/mart/thanks/<?php foreach ($this->cart->contents() as $items) : ?>
-	<?php echo $items['id'] ?> <?php endforeach ?>" class="btn btn-primary">Bayar Sekarang</a>
-  </div>
-</div>
-</form>
-
-      </div>
-
-      <div class="col-md-4">
-        <div class="card">
-  <div class="card-header text-center" style="background-color: green; color:white;">
-   <h3> Detail Pembelian</h3>
-  </div>
-  <div class="card-body descProdukDetail" style="border:0;">
-  <h5 class="card-title">Sub Total (<?php echo $this->cart->total_items(); ?> Items) :  <span>Rp <?php echo $this->cart->total(); ?></span></h5>
-    <h5 class="card-title">Biaya Pengiriman:  <span>Rp 5.000</span></h5>
-    <h5 class="card-title">Total: <span>Rp <?echo Rp 55.000</span></h5>
-  </div>
-</div>
-    </div>
-    
-
-</section>
-
-  <?php include_once("template/footer.php"); ?> 
-  
-</body>
-</html>
+    <section id="formPembayaranProduk">
+      <div class="container-fluid" style="padding: 5%;">
+        <div class="row">
+          <div class="col-md-7">
+            <h2 style="text-align: center;">Form Pembayaran Produk</h2>
+            <hr class="col-md-4">
+            <br>
+            <form class="form-horizontal" method = "post" action="<?php echo base_url();?>index.php/mart/thanks/<?php foreach ($this->cart->contents() as $items) : ?>
+              <?php echo $items['id'] ?> <?php endforeach ?>">
+              <div class="form-group">
+                <label class="control-label col-md-4" for="produk">Metode Pembayaran</label>
+                <div class="col-md-12">
+                  <select class="form-control" id="sel1" placeholder="Metode Pembayaran">
+                    <option>ATM</option>
+                  </select>
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="control-label col-md-4" for="deskripsi">Pilih Bank</label>
+                <div class="col-md-12">
+                  <select class="form-control" id="sel1" placeholder="Kategori">
+                    
+                    <option>CIMB NIAGA</option>
+                    <option>Mandiri</option>
+                    <option>BCA</option>
+                    <option>BRI</option>
+                    <option>BNI</option>
+                  </select>
+                </div>
+              </div>
+              
+              
+              <br>
+              <h2>Pengiriman</h2>
+              <br>
+              <div class="form-group">
+                <label class="control-label col-md-4" for="produk">Alamat Pengiriman</label>
+                
+                <div class="col-md-12">
+                  <textarea name="alamat" class="form-control"> </textarea>
+                </div>
+              </div>
+              
+              <div class="form-group">
+                <label class="control-label col-md-4" for="produk">Pilih Propinsi</label>
+                <div class="col-md-12">
+                  <select class="form-control" id="provinsi" placeholder="Jasa Pengiriman">
+                    
+                    <option>Jawa Timur</option>
+                    <option>Jawa Barat</option>
+                    <option>Jawa Tengah</option>
+                    <option>DKI Jakarta</option>
+                    <option>DI Yogyakarta</option>
+                    <option>Banten</option>
+                  </select>
+                </div>
+              </div>
+              
+              <div class="form-group">
+                <label class="control-label col-md-4" for="produk">Pilih Jasa Pengiriman</label>
+                <div class="col-md-12">
+                  <select class="form-control" id="sel1" placeholder="Jasa Pengiriman">
+                    <option>JNE</option>
+                    <option>TIKI</option>
+                    <option>J&T</option>
+                  </select>
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="control-label col-md-4" for="produk">Paket Pengiriman</label>
+                <div class="col-md-12">
+                  <select class="form-control" id="sel1" placeholder="Paket Pengiriman">
+                    <option>REG</option>
+                    <option>YES</option>
+                    <option>OKE</option>
+                  </select>
+                </div>
+              </div>
+              
+              
+              
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-md-12">
+                  <a href="<?php echo base_url();?>index.php/mart/thanks/<?php foreach ($this->cart->contents() as $items) : ?>
+                  <?php echo $items['id'] ?> <?php endforeach ?>" class="btn btn-primary">Bayar Sekarang</a>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="col-md-4">
+            <div class="card">
+              <div class="card-header text-center" style="background-color: green; color:white;">
+                <h3> Detail Pembelian</h3>
+              </div>
+              <div class="card-body descProdukDetail" style="border:0;">
+                <h5 class="card-title">Sub Total (<?php echo $this->cart->total_items(); ?> Items) :  <span>Rp <?php echo $this->cart->total(); ?></span></h5>
+                <h5 class="card-title">Biaya Pengiriman :  <span>Rp 5.000</span></h5>
+                <h5 class="card-title">Total : <span>Rp 55.000</span></h5>
+              </div>
+            </div>
+          </div>
+          
+        </section>
+        <?php include_once("template/footer.php"); ?>
+        
+      </body>
+    </html>
