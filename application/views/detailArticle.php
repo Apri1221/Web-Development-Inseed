500bc53f4a0743a31ab443b6f535472cfbea6e01
Tj_Julian <timothy_julian@ymail.com>
2018-09-20 19:57:53 +0700

    article "terisi"

--- application/views/detailArticle.php
+++ application/views/detailArticle.php
@@ -3,7 +3,7 @@
   <head>
     
     <?php include_once("template/header.php"); ?>
-    <title>inseed.id - Selamat Datang</title>
+    <title>inseed.id - Artikel </title>
     
   </head>
   <body id="page-top">
@@ -15,7 +15,7 @@
         <div class="row">
           <div class="col-lg-12 col-md-12 col-sm-12 text-center" id="featur">
             <h1 class="text-uppercase text-center" >
-            <h2>Kisah Petani Bone memulai Budidaya Rumput Laut</h2>
+            <h2>Jenis dan Manfaat Rumput Laut di Indonesia</h2>
             </h1>
             <hr class="my-4">
           </div>
@@ -27,12 +27,15 @@
         </div>
         <br>
         <div class="row">
-          <p class="col-lg-12 col-md-12 col-sm-12" style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p class="col-lg-12 col-md-12 col-sm-12" style="text-align: justify;">Berbagai negara maritim, Indonesia mempunyai potensi besar dalam memanfaatkan berbagai jenis rumput laut yang hidup di perairannya. Berbagai jenis rumput laut telah dikenal memiliki manfaat baik sebagai bahan pembuat agar-agar, keragian, maupun alginat. Berbagai jenis rumput laut pun telah berhasil dibudidayakan di pelbagai wilayah Indonesia.
+<br><br>
+Rumput laut adalah nama umum untuk menyebut pelbagai jenis organisme laut yang dikenal sebagai alga. Penggunaan istilah rumput laut kadang menimbulkan kerancuan karena dipakai untuk menyebut dua kelompok tanaman laut yang berbeda, yaitu lamun (seagrass) dan gulma laut (seaweed). Justru di Indonesia kata rumput laut lebih sering digunakan untuk menyebut gulma laut (seaweed). Meskipun secara botani alga bukan golongan rumput-rumputan.
+<br><br>
+Dalam artikel jenis dan manfaat rumput laut di Indonesia ini pun, yang dimaksud dan akan dibahas adalah rumput laut sebagai seaweed atau gulma laut. Mengingat di masyarakat istilah ‘gulma laut’ kurang populer.
+<br><br>
+Rumput laut (atau lebih tepatnya gulma laut) adalah alga makroskopik yang hidup di perairan. Layaknya alga lainnya, rumput laut tidak memiliki akar, batang, dan daun sejati. Seluruh bagian rumput laut disebut talus (thallus). Talus pada rumput laut ada yang tanpa percabangan dan bercabang-cabang dengan sifat mulai dari lunak, keras (diliputi zat kapur), seperti tulang rawan, hingga berserabut.
+<br><br>
+Karena tidak memiliki akar, rumput laut hidup dengan menempel pada substrat (fitobintes) baik pasir, lumpur, kayu, karang mati, maupun kulit kerang. Rumput laut hidup di perairan laut dangkal hingga kedalaman 200 meter. Daerah persebarannya mulai dari perairan beriklim tropis, subtropis, hingga perairan dingin.</p>
         </div>
 
         <div class="col-lg-8 col-md-8 col-sm-8 text-center" style="margin: 0 15% 0;">
@@ -40,11 +43,10 @@
             <div class="card">
               <div class="card-body row">
                <div class="col-md-3">
-                 <img src="<?php echo base_url('asset/assets/image/petani/petani1.jpg'); ?>" style="max-width: 80px; height: auto;">
+                 <img src="<?php echo base_url('asset/assets/image/jul.jpg'); ?>" style="max-width: 80px; height: auto;">
                </div>
                <div class="col-md-9" style="text-align: justify;">
-                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                   tempor incididunt ut labore et dolore magna aliqua. .</p>
+                   <p>Mantap kak artikel nya aku suka</p>
                    <footer class="blockquote-footer">Julian</footer>
                </div>
               
