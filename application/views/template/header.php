700152eaf6413c170466c27dfffa2b93a382edd2
apri <apriyantotobing8@gmail.com>
2018-09-20 10:45:11 +0700

    chat

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -19,3 +19,16 @@
     $("#loading").fadeOut("slow");
     });
     </script>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5828a2bee2def07b70859698/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
