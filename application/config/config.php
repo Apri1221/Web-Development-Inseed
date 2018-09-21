47612b967533e50e67c093309971f32b9eedf01a
apri <apriyantotobing8@gmail.com>
2018-09-21 19:27:46 +0700

    somting not important

--- application/config/config.php
+++ application/config/config.php
@@ -36,7 +36,7 @@ str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
