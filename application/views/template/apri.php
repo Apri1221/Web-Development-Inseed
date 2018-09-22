0ed758afa5bc8d6fa3767611e086425dcc28b276
apri <apriyantotobing8@gmail.com>
2018-09-23 01:31:52 +0700

    msh blm cepat

--- application/views/template/apri.php
+++ application/views/template/apri.php
@@ -59,8 +59,9 @@ function getPersentase($iniaddress, $ekspUntung){
 	{
 		$ch = curl_init();
 		$timeout = 5;
-		curl_setopt($ch,CURLOPT_URL,$url);
 		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch,CURLOPT_URL,$url);
 		curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
 		$data = curl_exec($ch);
 		curl_close($ch);
