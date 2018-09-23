68521fac2aee41973242d328270625e4add0fe04
apri <apriyantotobing8@gmail.com>
2018-09-23 21:42:11 +0700

    artikel

--- application/views/article.php
+++ application/views/article.php
@@ -25,25 +25,16 @@
                 <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
               </ol>
               <div class="carousel-inner" id="carouselArticle" >
+                
+                
                 <div class="carousel-item active">
                   <a href="<?=base_url()?>index.php/article/detail"> 
                     <img class="d-block w-100" src="<?php echo base_url('asset/assets/image/petani/petani1.jpg'); ?>" alt="First slide">
                     <h5 class="caption">Jenis dan Manfaat Rumput Laut di Indonesia</h5>
                   </a>
                 </div>
-                <div class="carousel-item">
-                  <a href="<?=base_url()?>index.php/article/detail"> 
-                    <img class="d-block w-100" src="<?php echo base_url('asset/assets/image/petani/petani2.jpg'); ?>" alt="Second slide">
-                    <h5 class="caption">Ditemukan rumput laut langka menghebohkan netizen</h5>
-                  </a>
-                </div>
-                <div class="carousel-item">
-                  <a href="<?=base_url()?>index.php/article/detail"> 
-                    <img class="d-block w-100" src="<?php echo base_url('asset/assets/image/petani/petani3.jpg'); ?>" alt="Third slide">
-                    <h5 class="caption">Bapak ini menemukan harta karun isinya rumput laut</h5>
-                  </a>
-                </div>
-
+                
+                
               </div>
               <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
